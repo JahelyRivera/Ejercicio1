39005af3eab4ce2dd4231860779b31a67d221688
DELL <DELL@DESKTOP-FUNN0TB>
2022-12-02 18:06:44 -0600

    Agregar archivos de proyecto.

---
+++ EjemploClase/EjemploClase/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EjemploClase
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSuma_Clicked(object sender, EventArgs e)
+        {
+            var atributo = new Models.Atributos
+            {
+                N1 = Convert.ToInt32(TxtN1.Text),
+                N2 = Convert.ToInt32(TxtN2.Text),
+                Resultado = Convert.ToInt32(TxtN1.Text) + Convert.ToInt32(TxtN2.Text)
+            };
+
+            var resul = new Views.Resultado();
+            resul.BindingContext = atributo;
+            await Navigation.PushModalAsync(resul);
+        }
+
+        private async void BtnResta_Clicked(object sender, EventArgs e)
+        {
+            var atributo = new Models.Atributos
+            {
+                N1 = Convert.ToInt32(TxtN1.Text),
+                N2 = Convert.ToInt32(TxtN2.Text),
+                Resultado = Convert.ToInt32(TxtN1.Text) - Convert.ToInt32(TxtN2.Text)
+            };
+
+            var resul = new Views.Resultado();
+            resul.BindingContext = atributo;
+            await Navigation.PushModalAsync(resul);
+        }
+
+        private async void BtnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            var atributo = new Models.Atributos
+            {
+                N1 = Convert.ToInt32(TxtN1.Text),
+                N2 = Convert.ToInt32(TxtN2.Text),
+                Resultado = Convert.ToInt32(TxtN1.Text) * Convert.ToInt32(TxtN2.Text)
+            };
+
+            var resul = new Views.Resultado();
+            resul.BindingContext = atributo;
+            await Navigation.PushModalAsync(resul);
+        }
+
+        private async void BtnDivision_Clicked(object sender, EventArgs e)
+        {
+            var atributo = new Models.Atributos
+            {
+                N1 = Convert.ToInt32(TxtN1.Text),
+                N2 = Convert.ToInt32(TxtN2.Text),
+                Resultado = Convert.ToInt32(TxtN1.Text) / Convert.ToInt32(TxtN2.Text)
+            };
+
+            var resul = new Views.Resultado();
+            resul.BindingContext = atributo;
+            await Navigation.PushModalAsync(resul);
+        }
+    }
+}
---
+++ EjemploClase/EjemploClase/Models/Atributos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploClase.Models
+{
+    public class Atributos
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; } 
+        public int Resultado { get; set; }
+    }
+}
